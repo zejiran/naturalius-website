82515cbcb2026a5bb92a658cecccccd3742da792
Juan Alegría <juanszalegria@gmail.com>
2021-07-18 12:20:48 -0500

    Style: modify header references

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -43,7 +43,7 @@ export default Vue.extend({
     sections: [
       {
         text: 'Home',
-        href: '/',
+        href: '#',
       },
       {
         text: 'About Us',
--- src/components/Track.vue
+++ src/components/Track.vue
@@ -1,21 +1,22 @@
 <template>
   <v-container id="track" align-start fluid justify-center>
     <v-row>
-      <v-col class="text-center definition" cols="12" md="6">
+      <v-col class="definition" cols="12" md="6">
         <p class="pt-16 pb-5 text-left text-h3 text-md-h2">Who we are?</p>
         <p class="text-justify">Naturalius is a Latin American startup that revolutionizes the design and discovery of
           new molecules through biotechnology. We have an interdisciplinary group of experts in biology, chemistry,
           informatics, and engineering focused on satisfying specific needs of our clients.
         </p>
+        <!--TODO: <v-btn class="text-left" color="secondary" elevation="5">Our team</v-btn>-->
       </v-col>
       <v-col class="text-center" cols="12" md="6">
-          <lottie-animation
-              ref="Naturalius Definition"
-              :animationData="require('@/assets/lottie/programming.json')"
-              :autoPlay="true"
-              :loop="true"
-              :speed="1"
-          />
+        <lottie-animation
+            ref="Naturalius Definition"
+            :animationData="require('@/assets/lottie/programming.json')"
+            :autoPlay="true"
+            :loop="true"
+            :speed="1"
+        />
       </v-col>
     </v-row>
   </v-container>
