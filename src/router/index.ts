53cdc098fcf6126d6e8c18848db3005317bfec81
Juan Alegría <juanszalegria@gmail.com>
2021-07-31 18:24:11 -0500

    Feat: update team assets and router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,27 @@
 import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import VueRouter, {RouteConfig} from 'vue-router'
 import Home from '../views/Home.vue'
+import Team from '../views/Team.vue'
 
 Vue.use(VueRouter)
 
 const routes: Array<RouteConfig> = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  }
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/team',
+        name: 'Team',
+        component: Team
+    }
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
---
+++ src/views/Team.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <Header/>
+    <Contact/>
+    <Footer/>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Header from '../components/Header.vue'
+
+import Contact from '../components/Contact.vue'
+import Footer from '../components/Footer.vue'
+
+export default Vue.extend({
+  name: 'Team',
+
+  components: {
+    Header,
+    Contact,
+    Footer
+  },
+})
+</script>
