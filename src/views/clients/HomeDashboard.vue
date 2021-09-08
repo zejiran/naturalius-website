f015c867a80afcaaa0dffc3f840d28ed8ceb28d8
Juan Alegría <juanszalegria@gmail.com>
2021-09-08 18:18:05 -0500

    Feat: init clients login and dashboard

---
+++ src/views/clients/HomeDashboard.vue
@@ -0,0 +1,197 @@
+<template>
+  <v-app>
+    <v-container>
+      <v-navigation-drawer
+          v-model="drawer"
+          absolute
+          class="accent-4"
+          dark
+          style="background-color: #1C769D"
+          temporary
+      >
+        <v-list>
+          <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              link
+          >
+            <v-list-item-icon>
+              <v-icon>mdi-lock</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Configuration</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+
+        <template v-slot:append>
+          <div class="pa-2">
+            <v-btn block>
+              Logout
+            </v-btn>
+          </div>
+        </template>
+      </v-navigation-drawer>
+
+      <v-card>
+        <v-toolbar
+            color="#1C769D"
+            dark
+            flat
+        >
+          <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+
+          <v-toolbar-title>Your Dashboard</v-toolbar-title>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+
+          <template v-slot:extension>
+            <v-tabs
+                v-model="tab"
+                align-with-title
+            >
+              <v-tabs-slider color="yellow"></v-tabs-slider>
+
+              <v-tab
+                  v-for="item in items"
+                  :key="item"
+              >
+                {{ item }}
+              </v-tab>
+            </v-tabs>
+          </template>
+        </v-toolbar>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item
+              v-for="item in items"
+              :key="item"
+          >
+            <v-card flat>
+              <v-card-text v-text="text"></v-card-text>
+            </v-card>
+          </v-tab-item>
+        </v-tabs-items>
+      </v-card>
+
+      <div class="mt-16 ml-16 mb-16">
+        <h1 class="text-h3 font-weight-bold">
+          Welcome again Casa Luker!
+        </h1>
+      </div>
+
+      <v-card
+          class="my-16 mx-auto"
+          width="800"
+      >
+        <v-sheet
+            class="v-sheet--offset mx-auto"
+            color="#1C769D"
+            elevation="12"
+        >
+          <v-sparkline
+              :labels="labels"
+              :value="value"
+              color="white"
+              line-width="2"
+              padding="16"
+          ></v-sparkline>
+        </v-sheet>
+
+        <v-card-text class="pt-0">
+          <div class="text-h6 font-weight-light mb-2">
+            Biochemtion Data
+          </div>
+          <div class="subheading font-weight-light grey--text">
+            Last ML Analysis Results
+          </div>
+          <v-divider class="my-2"></v-divider>
+          <v-icon
+              class="mr-2"
+              small
+          >
+            mdi-clock
+          </v-icon>
+          <span class="text-caption grey--text font-weight-light">last container running 26 minutes ago</span>
+        </v-card-text>
+      </v-card>
+
+      <div class="mt-16 mr-16 ml-16 mb-16">
+        <h1 class="text-h4 font-weight-bold">
+          Your progress
+        </h1>
+      </div>
+
+      <v-container style="width: 500px; height: 200px">
+        <v-sparkline
+            :fill="fill"
+            :gradient="selectedGradient"
+            :line-width="width"
+            :padding="padding"
+            :smooth="radius || false"
+            :value="value"
+            auto-draw
+        ></v-sparkline>
+      </v-container>
+    </v-container>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+const gradients = [
+  ['#222'],
+  ['#42b3f4'],
+  ['red', 'orange', 'yellow'],
+  ['purple', 'violet'],
+  ['#00c6ff', '#F0F', '#FF0'],
+  ['#f72047', '#ffd200', '#1feaea'],
+]
+
+export default Vue.extend({
+  name: "HomeDashboard",
+
+  data: () => ({
+    fill: true,
+    drawer: false,
+    selectedGradient: gradients[4],
+    gradients,
+    padding: 2,
+    radius: 10,
+    value: [0, 2, 5, 9, 5, 10, 3, 5, 0, 0, 1, 8, 2, 9, 0],
+    width: 5,
+    labels: [
+      '12am',
+      '3am',
+      '6am',
+      '9am',
+      '12pm',
+      '3pm',
+      '6pm',
+      '9pm',
+    ],
+    tab: null,
+    items: [
+      'web', 'shopping', 'videos', 'images', 'news',
+    ],
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  }),
+})
+</script>
+
+<style scoped>
+.v-sheet--offset {
+  top: -24px;
+  position: relative;
+}
+</style>
---
+++ src/views/clients/Login.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fill-height fluid>
+        <v-layout align-center justify-center>
+          <v-flex md4 sm8 xs12>
+            <v-card class="elevation-12">
+              <v-toolbar color="#1C769D" dark>
+                <v-toolbar-title>Naturalius Client Login</v-toolbar-title>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                      label="Login"
+                      name="login"
+                      prepend-icon="mdi-account"
+                      type="text"
+                  ></v-text-field>
+                  <v-text-field
+                      id="password"
+                      label="Password"
+                      name="password"
+                      prepend-icon="mdi-lock"
+                      type="password"
+                  ></v-text-field>
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="#1C769D" dark to="/dashboard">Login</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: 'Login',
+});
+</script>
+
+<style scoped>
+.theme--light.v-application {
+  background-color: rgba(167, 218, 240, 0.25);
+}
+</style>
