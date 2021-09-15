d6aa3ef63aa170a2baa6bed6a4b733d02f0a42e9
Juan Alegría <juanszalegria@gmail.com>
2021-09-14 21:04:17 -0500

    Feat: add login validation

--- src/views/clients/Login.vue
+++ src/views/clients/Login.vue
@@ -1,6 +1,24 @@
 <template>
   <v-app>
     <v-content>
+      <v-alert
+          v-if="!validLogin"
+          dense
+          outlined
+          style="position: absolute; margin: 20px 30px"
+          type="error"
+      >
+        Incorrect user or password
+      </v-alert>
+      <v-alert
+          v-if="validLogin && login.length > 3 && password.length > 3"
+          dense
+          outlined
+          style="position: absolute; margin: 20px 30px"
+          type="success"
+      >
+        Click login button for being redirected
+      </v-alert>
       <v-container fill-height fluid>
         <v-layout align-center justify-center>
           <v-flex md4 sm8 xs12>
@@ -11,6 +29,7 @@
               <v-card-text>
                 <v-form>
                   <v-text-field
+                      v-model="login"
                       label="Login"
                       name="login"
                       prepend-icon="mdi-account"
@@ -18,6 +37,7 @@
                   ></v-text-field>
                   <v-text-field
                       id="password"
+                      v-model="password"
                       label="Password"
                       name="password"
                       prepend-icon="mdi-lock"
@@ -27,7 +47,7 @@
               </v-card-text>
               <v-card-actions>
                 <v-spacer></v-spacer>
-                <v-btn color="#1C769D" dark to="/dashboard">Login</v-btn>
+                <v-btn color="#1C769D" dark @click="goToDashboard">Login</v-btn>
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -42,11 +62,29 @@ import Vue from "vue";
 
 export default Vue.extend({
   name: 'Login',
-});
+
+  data: () => ({
+    login: '',
+    password: '',
+    validLogin: true,
+  }),
+  methods: {
+    goToDashboard() {
+      if (this.login == 'luker' && this.password == 'casa') {
+        this.validLogin = true
+        setTimeout(() => {
+          //
+        }, 200)
+        this.$router.push('/dashboard');
+        window.scrollTo(0, 0)
+      } else {
+        this.validLogin = false
+      }
+    }
+  }
+})
+
 </script>
 
 <style scoped>
-.theme--light.v-application {
-  background-color: rgba(167, 218, 240, 0.25);
-}
 </style>
